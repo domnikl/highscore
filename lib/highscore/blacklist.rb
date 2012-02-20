91f8c40a50a75f022aea26639834d76fe4314438
Dominik Liebler <liebler.dominik@googlemail.com>
2012-02-20 19:45:37 +0100

    #11 add a whitelist
    refactored blacklist and moved common parts to a new class Wordlist and changed tests accordingly.

--- lib/highscore/blacklist.rb
+++ lib/highscore/blacklist.rb
@@ -1,15 +1,12 @@
+$:.unshift(File.dirname(__FILE__))
+require "wordlist"
+
 module Highscore
 
   # blacklisted words to be ignored in the resulting keywords
   #
-  class Blacklist
+  class Blacklist < Wordlist
     include Enumerable
-
-    # load a file of keywords
-    def self.load_file file_path
-      words = File.read(file_path).split(' ')
-      self.load(words)
-    end
 
     # load default file
     #
@@ -17,56 +14,5 @@ module Highscore
       file_path = File.join(File.dirname(__FILE__), %w{.. blacklist.txt})
       self.load_file(file_path)
     end
-
-    # load a file or array of words
-    #
-    def self.load(data)
-      if data.instance_of?(String)
-        words = data.split(' ')
-      elsif data.instance_of? Array
-        words = data
-      else
-        raise ArgumentError, "don't know how to handle a %s class" % data.class
-      end
-
-      words.map! {|x| x.gsub(/[\!\.\:\,\;\-\+]/, '') }
-
-      self.new(words)
-    end
-
-    attr_reader :words
-
-    def initialize(words = [])
-      @words = words
-    end
-
-    # iterate over words
-    #
-    def each
-      @words.each {|x| yield x }
-    end
-
-    # count of ignored words
-    def length
-      @words.length
-    end
-
-    # get an array of blacklisted words
-    #
-    def to_a
-      @words.to_a
-    end
-
-    # does the blacklist contain this keyword?
-    #
-    def include? keyword
-      @words.include? keyword
-    end
-
-    # add a new word to the blacklist
-    def <<(word)
-      @words << word
-    end
-
   end
 end
---
+++ lib/highscore/wordlist.rb
@@ -0,0 +1,63 @@
+module Highscore
+
+  # a basic list of words
+  class Wordlist
+    include Enumerable
+
+    # load a file of keywords
+    def self.load_file file_path
+      words = File.read(file_path).split(' ')
+      self.load(words)
+    end
+
+    # load a file or array of words
+    #
+    def self.load(data)
+      if data.instance_of?(String)
+        words = data.split(' ')
+      elsif data.instance_of? Array
+        words = data
+      else
+        raise ArgumentError, "don't know how to handle a %s class" % data.class
+      end
+
+      words.map! {|x| x.gsub(/[\!\.\:\,\;\-\+]/, '') }
+
+      self.new(words)
+    end
+
+    attr_reader :words
+
+    def initialize(words = [])
+      @words = words
+    end
+
+    # iterate over words
+    #
+    def each
+      @words.each {|x| yield x }
+    end
+
+    # count of ignored words
+    def length
+      @words.length
+    end
+
+    # get an array of blacklisted words
+    #
+    def to_a
+      @words.to_a
+    end
+
+    # does the blacklist contain this keyword?
+    #
+    def include? keyword
+      @words.include? keyword
+    end
+
+    # add a new word to the blacklist
+    def <<(word)
+      @words << word
+    end
+  end
+end
